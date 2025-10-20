84bd3e95de899e3f7e6777308171eacd355b0f08
hsh980609 <130471656+hsh980609@users.noreply.github.com>
2025-10-20 14:18:23 +0900

    큰틀에 필요한 함수 생성

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from pico2d import *
 
 
+
+
+
+def handle_events():
+    pass
+
+
+def update_world():
+    pass
+
+
+def render_world():
+    pass
+
+def reset_world():
+    pass
+
+
 open_canvas()
 reset_world()
 
