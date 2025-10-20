b83b307dde319c9893680db8552d603614b6a361
hsh980609 <130471656+hsh980609@users.noreply.github.com>
2025-10-20 17:46:18 +0900

    커비 클래스 생성 커비의 행동인 idle,run,fly,suction 클래스 생성

---
+++ kirby.py
@@ -0,0 +1,78 @@
+from pico2d import *
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK_a
+
+
+class Idle:
+    def __init__(self):
+        pass
+
+    def enter(self):
+        pass
+
+    def do(self):
+        pass
+
+    def exit(self):
+        pass
+
+
+
+
+class Run:
+    def __init__(self):
+        pass
+
+    def enter(self):
+        pass
+
+    def do(self):
+        pass
+
+    def exit(self):
+        pass
+
+
+class Fly:
+    def __init__(self):
+        pass
+
+    def enter(self):
+        pass
+
+    def do(self):
+        pass
+
+    def exit(self):
+        pass
+
+
+class Suction:
+    def __init__(self):
+        pass
+
+    def enter(self):
+        pass
+
+    def do(self):
+        pass
+
+    def exit(self):
+        pass
+
+
+class Kirby:
+    def __init__(self):
+        self.IDLE = Idle(self)
+        self.RUN = Run(self)
+        self.FLY = Fly(self)
+        self.SUCTION = Suction(self)
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def handle_event(self):
+        pass
