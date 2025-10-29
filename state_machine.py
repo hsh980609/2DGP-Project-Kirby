a9dc6362364f4302fcb05e8983576b276e83220a
hsh980609 <130471656+hsh980609@users.noreply.github.com>
2025-10-29 20:36:14 +0900

    상태 머신 생성자 수정

--- state_machine.py
+++ state_machine.py
@@ -1,9 +1,11 @@
 from event_to_string import event_to_string
 
 class StateMachine:
-    def __init__(self, start_state):
+    def __init__(self, start_state, state_transitions):
         self.cur_state = start_state
-        pass
+        self.state_transitions = state_transitions
+        self.cur_state.enter(('START', None))
+
     def update(self):
         self.cur_state.do()
 
@@ -11,5 +13,8 @@ class StateMachine:
         self.cur_state.draw()
 
 
-    def handle_state_event(self):
+    def handle_state_event(self,event):
+        for check_event in self.state_transitions[self.cur_state].keys():
+            if check_event(event):
+                pass
         pass
