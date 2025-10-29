76b0eaf9fe7fb8d9692f6dc72d071fc2e96f5f5e
hsh980609 <130471656+hsh980609@users.noreply.github.com>
2025-10-29 20:41:25 +0900

    state_transitions 일부 작성

--- kirby.py
+++ kirby.py
@@ -125,7 +125,13 @@ class Kirby:
         self.state_machine = StateMachine(
             # self.IDLE,
             # self.RUN
-            self.JUMP
+            self.JUMP,
+            {
+                self.IDLE :{right_down: self.RUN, left_down: self.RUN, right_up: self.RUN, left_up: self.RUN},
+                self.RUN :{right_up: self.IDLE, left_up: self.IDLE, right_down: self.IDLE, left_down: self.IDLE},
+
+             }
+
         )
 
 
