e4d3078a3ea2f2d747d60b8b1fcefde22c31ca2b
hsh980609 <130471656+hsh980609@users.noreply.github.com>
2025-11-05 20:13:44 +0900

    main 변경

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 from pico2d import *
-import play_mode
+import game_framework
+import play_mode as start_mode
 
 
 
 open_canvas()
-play_mode.init()
-
-while play_mode.running:
-    play_mode.handle_events()
-    play_mode.update()
-    play_mode.draw()
-    delay(0.01)
-
-play_mode.finish()
+game_framework.run(start_mode)
 close_canvas()
