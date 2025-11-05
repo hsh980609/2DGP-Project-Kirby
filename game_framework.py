67e159a2f0e45587923a97f8e19690659d4f3e45
hsh980609 <130471656+hsh980609@users.noreply.github.com>
2025-11-05 20:12:32 +0900

    time 추가 및 리팩토링

--- game_framework.py
+++ game_framework.py
@@ -1,25 +1,24 @@
-from play_mode import running
-
+import time
 frame_time = 0.0
 
 running = None
 stack = None
 
-def change_mode(mode):
+def change_mode(mode): # 현재 모드 삭제하고 새로운 모드 추가하면서 init 호출
     global stack
     if (len(stack) > 0):
         stack[-1].pause()
     stack.append(mode)
     mode.init()
 
-def push_mode(mode):
+def push_mode(mode): # 현재 모드 pause하고 새로운 모드를 스택에 추가 후 init 호출
     global stack
     if (len(stack) > 0):
         stack[-1].pause()
     stack.append(mode)
     mode.init()
 
-def pop_mode():
+def pop_mode(): # 현재 모드 finish 호출 후 현재모드 제거. 이제 Top에는 이전모드가 존재하므로 resume 호출
     global stack
     if (len(stack) > 0):
         stack[-1].finish()
@@ -37,10 +36,14 @@ def quit():
 def run(start_mode):
     global running, stack
     running = True
-    stack = [start_mode]
+    stack = [start_mode] # 스택에 시작 모드 추가
     start_mode.init()
 
-    while running:
+    while running: # 현재 모드에 대한 루프 실행
         stack[-1].handle_events()
         stack[-1].update()
         stack[-1].draw()
+
+    while (len(stack) > 0): # 스택에 남은 모드들 차례대로 삭제
+        stack[-1].finish()
+        stack.pop()
