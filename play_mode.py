7004be2559bad3c5b9ef52430e192feb45714e71
hsh980609 <130471656+hsh980609@users.noreply.github.com>
2025-11-05 19:46:27 +0900

    main 일부 play_mode쪽으로 리팩토링

---
+++ play_mode.py
@@ -0,0 +1,40 @@
+from pico2d import *
+
+import game_world
+from kirby import Kirby
+
+kirby = None
+running = True
+
+def handle_events():
+    global running
+
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            kirby.handle_event(event)
+
+def init_game():
+    global kirby
+    global running
+
+    running = True
+
+    kirby = Kirby()
+    game_world.add_object(kirby, 1)
+
+def update():
+    game_world.update()
+
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def finish_game():
+    pass
