f0e107e66bc075730c0e2a6e965e8de1ad6fc7f0
hsh980609 <130471656+hsh980609@users.noreply.github.com>
2025-11-05 20:01:51 +0900

    필요한 함수들 생성

---
+++ game_framework.py
@@ -0,0 +1,22 @@
+from play_mode import running
+
+frame_time = 0.0
+
+running = None
+stack = None
+
+def change_mode(mode):
+    pass
+
+def push_mode(mode):
+    pass
+
+def pop_mode():
+    pass
+
+def quit():
+    global running
+    running = False
+
+def run(start_mode):
+    pass
