3ed47cb456c6a258e96b274185165722a495c805
hsh980609 <130471656+hsh980609@users.noreply.github.com>
2025-11-06 18:05:35 +0900

    배경화면 출력 수정

--- background.py
+++ background.py
@@ -2,12 +2,18 @@ from pico2d import *
 
 class Background:
     def __init__(self):
-        self.image = load_image('background.png')
+        self.image = load_image('Backgrounds.png')
+        full_w = self.image.w
+        full_h = self.image.h
 
+        self.clip_w = full_w // 3
+        self.clip_h = full_h // 3
+
+        self.clip_bottom = self.clip_h * 2
     def update(self):
         pass
 
     def draw(self):
-        self.image.draw(400, 100)
-        self.image.draw(1200, 30)
+        self.image.clip_draw(0, self.clip_bottom, self.clip_w, self.clip_h, 400, 300, 1000, 900)
+        #self.image.clip_draw(0,self.clip_bottom, self.clip_w, self.clip_h, 1200, 30)
 
