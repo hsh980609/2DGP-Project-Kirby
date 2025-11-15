dcf10f96d34df108d46e6f969b58b0d8e1d67292
hsh980609 <130471656+hsh980609@users.noreply.github.com>
2025-11-15 20:32:47 +0900

    add_collision_pair 구현

--- game_world.py
+++ game_world.py
@@ -38,7 +38,13 @@ def collide(a, b):
 collision_pairs = {}
 
 def add_collision_pair(group, a, b):
-    pass
+    if group not in collision_pairs:
+        print(f'Added new group {group}')
+        collision_pairs[group] = [[],[]]
+    if a:
+        collision_pairs[group][0].append(a)
+    if b:
+        collision_pairs[group][0].append(a)
 
 def handle_collision():
     pass
